45256f1686625574da843483fcec0e4bc90885b4
qasimmizbah <36446191+qasimmizbah@users.noreply.github.com>
2023-03-11 09:57:18 +0530

    Add files via upload

---
+++ excerpts-wp-pages.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Excerpts For Wordpress Pages
+ * Plugin URI:  https://www.upwork.com/freelancers/~01d00918bea0f63345
+ * Description: Add Excerpts For Wordpress Pages
+ * Version:     1.0
+ * Author:      Qasimmizbah
+ * Author URI:  https://www.upwork.com/freelancers/~01d00918bea0f63345
+ * License:     GPL-2.0+
+ */
+
+
+function myexcerpt() {
+  add_post_type_support('page', array('excerpt'));
+}
+add_action('init', 'myexcerpt');
---
+++ readme.txt
@@ -0,0 +1,17 @@
+=== Plugin Name ===
+Excerpts For Wordpress pages
+Tags: pages, excerpts
+
+This plugin adds excerpts for Pages
+
+
+== Description ==
+
+This plugin automatically adds excerpts for Pages
+
+
+== Installation ==
+
+1. Upload 'excerpts-wp-pages' to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Enjoy!
